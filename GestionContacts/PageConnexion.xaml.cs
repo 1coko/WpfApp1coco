707305186b23070b5d8a1ba55a6926ee6c7fd7c6
1coko <malek_45@hotmail.fr>
2022-09-01 00:04:06 +0200

    Ajoutez des fichiers projet.

---
+++ GestionContacts/PageConnexion.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestionContacts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string identification = "Coca";
+        string motDePasse = "Cola";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValiderPageConnexion_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtUserId.Text) || String.IsNullOrWhiteSpace(txtMotDePasse.Password))
+            {
+                MessageBox.Show("Les champs 'identification' et 'Mot de passe' doivent être renseignés");
+            }
+            else
+            {
+                if (txtUserId.Text == identification && txtMotDePasse.Password == motDePasse)
+                {
+                    MessageBox.Show("Bonjour,\nMerci de patienter, la connexion est en cours.");
+
+                    MainWindow page = new MainWindow();
+                    page.Show();
+
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Les champs 'identification' et ou 'mot de passe' sont incorrecte.\nMerci de resaisir les champs.");
+                }
+            }
+        }
+    }
+}
---
+++ GestionContacts/PageInscription.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestionContacts
+{
+    /// <summary>
+    /// Logique d'interaction pour Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInscription_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNomInscription.Text) || string.IsNullOrWhiteSpace(txtPrenomInscription.Text) || string.IsNullOrWhiteSpace(txtTelInscription.Text))
+            {
+                MessageBox.Show("Remplissez tous les champs !");
+            }
+            else
+            {/*
+                if (txtTelInscription.Length != longNumTel)
+                {
+                    MessageBox.Show("SVP, merci de saisir un numéro de téléphone à 8 chiffres");
+                    txtTelInscription.Text = string.Empty;
+                }
+                else
+                {
+                    txtListeInscrit.Text = txtListeInscrit.Text + "\n" + txtNomInscription.Text + " " + txtPrenomInscription.Text + " " + txtTelInscription.Text;
+
+
+                    txtNomInscription.Text = string.Empty;
+                    txtPrenomInscription.Text = string.Empty;
+                    txtTelInscription.Text = string.Empty;
+                }*/
+                txtlisteInscrit.Text = txtlisteInscrit.Text + "\n" + txtNomInscription.Text + " " + txtPrenomInscription.Text + " " + txtTelInscription.Text;
+
+
+                txtNomInscription.Text = string.Empty;
+                txtPrenomInscription.Text = string.Empty;
+                txtTelInscription.Text = string.Empty;
+            }
+        }
+    }
+}
